6614e245fa51bfeaf25becdb5f8d39b6b0d436bb
sofiene khannoussi <khannoussi.sofiene@gmail.com>
2024-06-28 00:04:46 +0200

    AVATAR ET CV

---
+++ backend/src/main/java/com/iteam/Gestion/Expert/configimage/FileStorageException.java
@@ -0,0 +1,11 @@
+package configimage;
+
+public class FileStorageException extends RuntimeException{
+    public FileStorageException(String message, Throwable cause){
+        super(message,cause);
+    }
+    public FileStorageException(String message){
+        super(message);
+    }
+
+}
